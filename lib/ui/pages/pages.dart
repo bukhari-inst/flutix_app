5f6efa76d6147ed8274e2dd72b6dafaa05a3e1e2
Bukhori Inst <58775021+bukhori-inst@users.noreply.github.com>
2021-03-25 01:21:27 +0700

    sign in part2

--- lib/ui/pages/pages.dart
+++ lib/ui/pages/pages.dart
@@ -1,10 +1,13 @@
 // import 'dart:html';
 
+import 'package:email_validator/email_validator.dart';
+import 'package:flushbar/flushbar.dart';
 import 'package:flutix_app/bloc/blocs.dart';
 import 'package:flutix_app/services/services.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutix_app/shared/shared.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
