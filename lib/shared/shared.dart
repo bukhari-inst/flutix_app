35f423a85a9180f6ee50f40faec7915b51dccefd
Bukhori Inst <58775021+bukhori-inst@users.noreply.github.com>
2021-03-24 18:25:56 +0700

    sett theme, color-font

--- lib/shared/shared.dart
+++ lib/shared/shared.dart
@@ -1,3 +1,6 @@
 import 'package:flutix_app/bloc/blocs.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 part 'shared_value.dart';
+part 'theme.dart';
