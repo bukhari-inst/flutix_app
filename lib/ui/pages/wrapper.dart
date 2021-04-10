329345b289ebb1653cc95c9cd482dfed11a97ca8
Bukhori Inst <58775021+bukhori-inst@users.noreply.github.com>
2021-04-10 20:40:19 +0700

    Preference Page part 1

--- lib/ui/pages/wrapper.dart
+++ lib/ui/pages/wrapper.dart
@@ -26,6 +26,8 @@ class Wrapper extends StatelessWidget {
                 ? SignInPage()
                 : (pageState is OnRegistrationPage)
                     ? SignUpPage(pageState.registrationData)
-                    : MainPage());
+                    : (pageState is OnpreferencePage)
+                        ? PreferencePage(pageState.registrationData)
+                        : MainPage());
   }
 }
