d73ef6aa3ec5364c5acadece4d6ef18a4a066d2a
Bukhori Inst <58775021+bukhori-inst@users.noreply.github.com>
2021-04-11 12:15:02 +0700

    upload profile image

---
+++ lib/shared/shared_methods.dart
@@ -0,0 +1,18 @@
+part of 'shared.dart';
+
+// upload profile image to movie page
+Future<File> getImage() async {
+  // ignore: deprecated_member_use
+  var image = await ImagePicker.pickImage(source: ImageSource.gallery);
+  return image;
+}
+
+Future<String> uploadImage(File image) async {
+  String fileName = basename(image.path);
+
+  StorageReference ref = FirebaseStorage.instance.ref().child(fileName);
+  StorageUploadTask task = ref.putFile(image);
+  StorageTaskSnapshot snapshot = await task.onComplete;
+
+  return await snapshot.ref.getDownloadURL();
+}
