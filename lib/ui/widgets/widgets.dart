a7cef5fd817bb9c849f15336f646d9f7163fd4f0
Bukhori Inst <58775021+bukhori-inst@users.noreply.github.com>
2021-05-20 22:07:01 +0700

    movie card & rating stars widget

--- lib/ui/widgets/widgets.dart
+++ lib/ui/widgets/widgets.dart
@@ -1,3 +1,7 @@
 import 'package:flutix_app/shared/shared.dart';
 import 'package:flutter/material.dart';
+import 'package:flutix_app/models/models.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 part 'selectable_box.dart';
+part 'movie_card.dart';
+part 'rating_stars.dart';
