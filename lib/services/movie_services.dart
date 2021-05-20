bc9acb659ba1dd20f44cab56295344bd91fcf113
Bukhori Inst <58775021+bukhori-inst@users.noreply.github.com>
2021-05-20 20:58:23 +0700

    fig bug typo! meresahkannn!!!!!!!!!!

--- lib/services/movie_services.dart
+++ lib/services/movie_services.dart
@@ -3,7 +3,7 @@ part of 'services.dart';
 class MovieServices {
   static Future<List<Movie>> getMovies(int page, {http.Client client}) async {
     String url =
-        "https://api.themoviedb.org/3/discover/movie?api_key=<<api_key=$apiKey&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=$page";
+        "https://api.themoviedb.org/3/discover/movie?api_key=$apiKey&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=$page";
 
     client ??= http.Client();
 
