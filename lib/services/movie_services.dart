fa1eab8315d2cc252a13aea161c45fa597614ea4
Bukhori Inst <58775021+bukhori-inst@users.noreply.github.com>
2021-05-23 00:41:56 +0700

    Movie details page part 2

--- lib/services/movie_services.dart
+++ lib/services/movie_services.dart
@@ -29,5 +29,47 @@ class MovieServices {
 
     var response = await client.get(url);
     var data = json.decode(response.body);
+
+    List genres = (data as Map<String, dynamic>)['genres'];
+    String language;
+
+    switch ((data as Map<String, dynamic>)['original_language'].toString()) {
+      case 'ja':
+        language = "Japanese";
+        break;
+      case 'id':
+        language = "Indonesian";
+        break;
+      case 'ja':
+        language = "Korean";
+        break;
+      case 'ja':
+        language = "English";
+        break;
+    }
+
+    return MovieDetail(movie,
+        language: language,
+        genres: genres
+            .map((e) => (e as Map<String, dynamic>)['name'].toString())
+            .toList());
+  }
+
+  static Future<List<Credit>> getCredits(int movieID,
+      {http.Client client}) async {
+    String url =
+        "https://api.themoviedb.org/3/movie/$movieID/credits?api_key=$apiKey";
+
+    client ??= http.Client();
+
+    var response = await client.get(url);
+    var data = json.decode(response.body);
+
+    return ((data as Map<String, dynamic>)['cast'] as List)
+        .map((e) => Credit(
+            name: (e as Map<String, dynamic>)['name'],
+            profilePath: (e as Map<String, dynamic>)['profile_path']))
+        .take(8)
+        .toList();
   }
 }
