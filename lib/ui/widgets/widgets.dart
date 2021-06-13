979fc9f73f1c967238f98de7da831ad1f5b12c1e
Bukhori Inst <58775021+bukhori-inst@users.noreply.github.com>
2021-06-13 12:32:48 +0700

    Date card widget

--- lib/ui/widgets/widgets.dart
+++ lib/ui/widgets/widgets.dart
@@ -2,6 +2,8 @@ import 'package:flutix_app/shared/shared.dart';
 import 'package:flutter/material.dart';
 import 'package:flutix_app/models/models.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:flutix_app/extensions/extensions.dart';
+
 part 'selectable_box.dart';
 part 'movie_card.dart';
 part 'rating_stars.dart';
@@ -9,3 +11,4 @@ part 'browse_button.dart';
 part 'coming_soon_card.dart';
 part 'promo_card.dart';
 part 'credit_card.dart';
+part 'date_card.dart';
