e2742163d696afdb7ed24c734c376b2b4983c3a7
Bukhori Inst <58775021+bukhori-inst@users.noreply.github.com>
2021-06-22 16:57:47 +0700

    Succes page finished

--- lib/ui/pages/success_page.dart
+++ lib/ui/pages/success_page.dart
@@ -17,22 +17,75 @@ class SuccessPage extends StatelessWidget {
               future: ticket != null
                   ? processingTicketOrder(context)
                   : processingTopUp(),
-              builder: (_, snapshot) =>
-                  (snapshot.connectionState == ConnectionState.done)
-                      ? Center(
+              builder: (_, snapshot) => (snapshot.connectionState ==
+                      ConnectionState.done)
+                  ? Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Container(
+                          width: 150,
+                          height: 150,
+                          margin: EdgeInsets.only(bottom: 70),
+                          decoration: BoxDecoration(
+                              image: DecorationImage(
+                                  image: AssetImage(ticket == null
+                                      ? "assets/top_up_done.png"
+                                      : "assets/ticket_done.png"))),
+                        ),
+                        Text(
+                          (ticket == null) ? "Emm Yummy!" : "Happy Watching!",
+                          style: blackTextFont.copyWith(fontSize: 20),
+                        ),
+                        SizedBox(height: 16),
+                        Text(
+                          (ticket == null)
+                              ? "You have successfully\ntop up the wallet"
+                              : "You have successfully\nbought the ticket",
+                          textAlign: TextAlign.center,
+                          style: blackTextFont.copyWith(
+                              fontSize: 16, fontWeight: FontWeight.w300),
+                        ),
+                        Container(
+                          height: 45,
+                          width: 250,
+                          margin: EdgeInsets.only(top: 70, bottom: 20),
                           // ignore: deprecated_member_use
                           child: RaisedButton(
-                            onPressed: () {
-                              context.bloc<PageBloc>().add(GoToMainPage());
-                            },
-                          ),
+                              elevation: 0,
+                              color: mainColor,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(8)),
+                              child: Text(
+                                (ticket == null) ? "My Wallet" : "My Tickets",
+                                style: whiteTextFont.copyWith(fontSize: 16),
+                              ),
+                              onPressed: () {}),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Text("Didcover new movie? ",
+                                style: greyTextFont.copyWith(
+                                    fontWeight: FontWeight.w400)),
+                            GestureDetector(
+                              onTap: () {
+                                context.bloc<PageBloc>().add(GoToMainPage());
+                              },
+                              child: Text(
+                                "Back to Home",
+                                style: purpleTextFont,
+                              ),
+                            )
+                          ],
                         )
-                      : Center(
-                          child: SpinKitFadingCircle(
-                            color: mainColor,
-                            size: 50,
-                          ),
-                        )),
+                      ],
+                    )
+                  : Center(
+                      child: SpinKitFadingCircle(
+                        color: mainColor,
+                        size: 50,
+                      ),
+                    )),
         ));
   }
 
